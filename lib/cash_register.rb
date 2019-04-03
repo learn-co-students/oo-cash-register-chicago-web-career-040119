980024e3497ad902c464207d21a7d0532d04939f
Jackson Robbins <jackrobbins1@gmail.com>
2019-04-03 17:30:28 -0500

    finished the apply_discount method

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
+require "pry"
 
+class CashRegister
+  attr_reader :total
+  attr_accessor :discount
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def total=(val)
+    @total = val
+  end
+
+  def add_item(title, price, qt = 1)
+    @total += price * qt
+  end
+
+  def apply_discount
+    if @discount == 0
+      return "There is no discount to apply."
+    else
+      disc = 1.00 - (@discount.to_f/100.to_f)
+      @total *= disc.to_f
+      "After the discount, the total comes to $#{@total.to_i}."
+    end
+  end
+
+end
+
+
+jack = CashRegister.new(10)
+#binding.pry
+wut = "wut"
