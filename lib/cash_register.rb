2836d7a2aa473a531e59c48885ca457f1669db6a
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-03-02 16:39:16 -0600

    refactored class methods

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,35 +1,34 @@
 class CashRegister
-attr_accessor :total, :discount, :items, :price
 
- def initialize(discount=0)
-   @total =  0
-   @discount = discount
-   @items = []
+  attr_accessor :items, :discount, :total, :last_transaction
 
- end
-
-def add_item(title, price, quantity = 1)
-  @total = @total + price*quantity
-  counter = 0
-  if quantity > 1 && counter < quantity
-    counter += 1
-    @items << title
-  else
-      @items << title
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
   end
-end
 
- def apply_discount
-   if discount > 0
-   @total -= @total * @discount/100
-   "After the discount, the total comes to $#{@total}."
-  else
-   "There is no discount to apply."
+  def total_amount(amount, quatity=1)
+    @total += amount * quatity
+  end 
+
+  def add_item(title, amount, quantity=1)
+    quantity.times do
+      items << title
+    end
+    @last_transaction = total_amount(amount, quantity)
   end
- end
 
- def void_last_transaction
-   @total.pop()
- end
+  def apply_discount
+    if discount != 0
+      @total = (total * ((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
+  end
 
+  def void_last_transaction
+    @total = @total - @last_transaction
+  end
 end
